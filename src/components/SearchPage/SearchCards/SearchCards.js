26527d953f2426b8730f8cf9ca8bac6ac17e5e0f
Thien Vo <vohuythien@utexas.edu>
2019-01-30 18:15:38 -0600

    Separated search results into own components and added component update check

--- src/components/SearchPage/SearchCards/SearchCards.js
+++ src/components/SearchPage/SearchCards/SearchCards.js
@@ -1,6 +1,48 @@
 import React from 'react';
+import SearchCard from './SearchCard/SearchCard';
+import { uniqueId } from 'lodash';
+import styles from './SearchCards.css';
+
+class SearchCards extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        const currentSongs = this.props.songs;
+        const nextSongs = nextProps.songs;
+        let changed = false;
+        if (currentSongs.length !== nextSongs.length) {
+            return true;
+        } else {
+            currentSongs.forEach((song, index) => {
+                let nextSong = nextSongs[index];
+                if (song.name !== nextSong.name || song.artists.length !== nextSong.artists.length) {
+                    changed = true;
+                    return;
+                } else {
+                    song.artists.forEach((artist, artistIndex) => {
+                        if (artist !== nextSong.artists[artistIndex]) {
+                            changed = true;
+                            return;
+                        }
+                    });
+                }
+            });
+        }
+        return changed;
+    }
 
 
-export default class SearchCards extends React.Component {
-    
+    render() {
+        //props => songs, onClick
+        const songCards = this.props.songs.map((song) => {
+            return (
+                <SearchCard key={uniqueId()} onClick={() => this.props.onClick(song.name, song.artists[0])} {...song}/>
+                );
+            });
+            return (
+                <div className={styles.songList}>
+                    {songCards}
+                </div>
+        );
+    };
 }
+
+export default SearchCards;
