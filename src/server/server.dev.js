d8e865447e4ea8be5df07e7268a83165bceea021
Thien Vo <vohuythien@utexas.edu>
2019-02-01 19:25:53 -0600

    Added express server

---
+++ src/server/server.dev.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const webpack = require('webpack');
+const config = require('../../webpack.config.dev.js');
+
+require('dotenv').load();
+
+const port = process.env.PORT ? process.env.PORT : 8080;
+const host = process.env.HOST ? process.env.HOST : '0.0.0.0';
+const app = express();
+const compiler = webpack(config);
+
+//middlewares
+const proxyMiddleware = require('http-proxy-middleware');
+const wdsMiddleware = require('webpack-dev-middleware');
+const hotMiddlware = require('webpack-hot-middleware')
+
+app.use(express.static(path.join(__dirname, '../../dist')))
+   .use(wdsMiddleware(compiler, {
+       publicPath: config.output.publicPath
+   }))
+   .use(hotMiddlware(compiler))
+   .use(cors())
+   .use('/api', proxyMiddleware('/api', { 
+        target: process.env.API_URL,
+        pathRewrite: {'^/api': ''},
+        logLevel: 'debug',
+        secure: false,
+        changeOrigin: true,
+    }));
+
+
+
+
+app.get('*', (req, res, next) => {
+    const filename = path.resolve(compiler.outputPath, 'index.html');
+    console.log(filename);
+    compiler.inputFileSystem.readFile(filename, (err, result) => {
+        if (err) return next(err);
+        res.set('content-type', 'text/html');
+        res.send(result);
+        res.end();
+    });
+});
+
+
+
+
+
+
+
+
+
+console.log('Listening on: ' + host + ':' + port);
+app.listen(port, host);
---
+++ src/server/server.prod.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const proxyMiddleware = require('http-proxy-middleware');
+const port = process.env.PORT ? process.env.PORT : 8080;
+const host = process.env.HOST ? process.env.HOST : '0.0.0.0';
+const webpack = require('webpack');
+const config = require('../../webpack.config.prod.js')(process.env);
+const app = express();
+const compiler = webpack(config);
+
+app.use(express.static(path.join(__dirname, '../../dist')))
+   .use(cors())
+   .use('/api', proxyMiddleware('/api', { 
+        target: process.env.API_URL,
+        pathRewrite: {'^/api': ''},
+        logLevel: 'info',
+        secure: false,
+        changeOrigin: true,
+}));
+
+app.get('*', (req, res, next) => {
+    const filename = path.resolve(compiler.outputPath, 'index.html');
+    compiler.inputFileSystem.readFile(filename, (err, result) => {
+        if (err) return next(err);
+        res.set('content-type', 'text/html');
+        res.send(result);
+        res.end();
+    });
+});
+
+
+console.log('Listening on: ' + host + ':' + port);
+app.listen(port, host);
