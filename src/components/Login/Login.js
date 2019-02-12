9beaed9f62e09093553002076fe96194c9a4019e
Thien Vo <vohuythien@utexas.edu>
2019-02-12 01:12:18 -0600

    Removed onclick for login, checks for logging in are made when song is searched

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './Login.css';
 
-const login = (props) => {
+const login = () => {
     return (
         <div className={styles.loginContainer}>
             <h3>PLEASE LOGIN TO SPOTIFY</h3>
             <a href={process.env.API_URL + '/login'} className={styles.buttonContainer}>
-                <div className={styles.loginButton} onClick={props.onClick}>Login</div>
+                <div className={styles.loginButton}>Login</div>
             </a>
         </div>
     );
