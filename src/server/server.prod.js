637799e00704f0210d4edc38d467acd230ff43f8
Thien Vo <vohuythien@utexas.edu>
2019-02-13 14:30:53 -0600

    Removed webpack from prod server

--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const proxyMiddleware = require('http-proxy-middleware');
 const port = process.env.PORT ? process.env.PORT : 8080;
 const host = process.env.HOST ? process.env.HOST : '0.0.0.0';
-const webpack = require('webpack');
-const config = require('../../webpack.config.prod.js')(process.env);
 const app = express();
-const compiler = webpack(config);
 
 function log(req, res, next) {
     console.log('Incoming request: ');
@@ -17,27 +13,12 @@ function log(req, res, next) {
     next();
 }
 
-function relayRequestHeaders(proxyReq, req) {
-    console.log('PROXY REQ');
-    console.log(proxyReq.headers);
-  }
-  
-  function relayResponseHeaders(proxyRes, req, res) {
-    console.log('PROXY RES');
-    console.log(proxyRes.headers);
-  }
 app
 .use(cors())
 .use(log).use(express.static(path.join(__dirname, '../../dist')));
 
-app.get('*', (req, res, next) => {
-    const filename = path.resolve(compiler.outputPath, 'index.html');
-    compiler.inputFileSystem.readFile(filename, (err, result) => {
-        if (err) return next(err);
-        res.set('content-type', 'text/html');
-        res.send(result);
-        res.end();
-    });
+app.get('/*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
 
